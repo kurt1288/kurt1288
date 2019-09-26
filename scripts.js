7e32fee265e306b39ac036527616f52b9e70cf72
Kurt <3249028+kurt1288@users.noreply.github.com>
2019-09-26 14:53:05 -0700

    updated site with new road.css. redid layout

--- scripts.js
+++ scripts.js
@@ -14,39 +14,9 @@ String.prototype.escapeHtml = function() {
 };
 
 window.onload = function() {
-    // Get current file URL
-    fetch('https://api.github.com/repos/kurt1288/road/tags').then(function(response) { 
-        return response.json();
-    }).then(function(myJson) {
-        const url = "https://github.com/kurt1288/Road/releases/download/" + myJson[0].name + "/road.zip";
-        Array.from(document.getElementsByClassName('downloadLink')).forEach(function(e) {
-            e.href = url;
-        });
-    });
-
     const codeElements = document.body.getElementsByTagName('code');
     for (let i=0; i < codeElements.length; i++) {
         const escapedHtml = codeElements[i].innerHTML.escapeHtml();
         codeElements[i].innerHTML = escapedHtml;
-    }
-
-    Array.from(document.getElementsByClassName('menuLink')).forEach(function(element) {
-        element.addEventListener('click', openMenu);
-    });
-
-    document.addEventListener('click', closeMenu);
-
-    function openMenu(e) {
-        e.preventDefault();
-        closeMenu();
-        let menu = document.getElementById(e.srcElement.dataset.menu);
-        menu.classList.add("open");
-        e.stopImmediatePropagation();
-    }
-
-    function closeMenu() {
-        if (document.getElementsByClassName('open').length > 0) {
-            document.getElementsByClassName('open')[0].classList.remove('open');
-        };
     }
 }
